862e822a41c9037cc957567f8b7d2d50496080af
amdudda <tk0654wm@go.minneapolis.edu>
2015-10-02 11:34:45 -0500

    further test of assignmt 6 fork

--- src/com/amdudda/Beverage.java
+++ src/com/amdudda/Beverage.java
@@ -5,5 +5,6 @@ package com.amdudda;
  */
 public class Beverage {
     // Object to store beverages sold at the coffeeshop
-
+    // I really don't get this forking thing... I keep expecting to
+    // see a separate file or tree or repository and just being confused.
 }
