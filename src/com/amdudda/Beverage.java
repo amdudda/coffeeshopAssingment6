ac0c87366b3c52b93bff9bb646e3d54e617fb7d2
amdudda <tk0654wm@go.minneapolis.edu>
2015-10-03 12:19:09 -0500

    Refactored Beverages >> Beverage Created initial set of attributes for object.

---
+++ src/com/amdudda/Beverage.java
@@ -0,0 +1,17 @@
+package com.amdudda;
+
+/**
+ * Created by anna on 10/3/15.
+ */
+public class Beverage {
+    // Beverage object to store data about beverages
+
+    // attributes of class beverage
+    // using primitives because there's no real reason as of this writing to wrap them
+    private String name;
+    private double cost;
+    private double price;
+    private int qty_sold;
+
+
+}
--- src/com/amdudda/Beverages.java
+++
@@ -1,7 +0,0 @@
-package com.amdudda;
-
-/**
- * Created by anna on 10/3/15.
- */
-public class Beverages {
-}
