6c1dae06fe1917f05e0b884bb0745d3f5e7fe70c
amdudda <tk0654wm@go.minneapolis.edu>
2015-10-03 12:25:07 -0500

    autogenerated a bunch of getter and setter methods for Beverage attributes

--- src/com/amdudda/Beverage.java
+++ src/com/amdudda/Beverage.java
@@ -21,4 +21,39 @@ public class Beverage {
         this.price = bevPrice;
         this.qty_sold = 0; // we won't know how many have been sold until user enters info.
     }  // end constructor
+
+    // a whole bunch of getter and setter methods.
+    // pro forma, because I think this assignment will only use set & get qty_sold.
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQty_sold() {
+        return qty_sold;
+    }
+
+    public void setQty_sold(int qty_sold) {
+        this.qty_sold = qty_sold;
+    }
+
 }
